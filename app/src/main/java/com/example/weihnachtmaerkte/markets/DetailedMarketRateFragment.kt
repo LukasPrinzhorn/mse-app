84348532a51b75cec3f2c276f1b2a025cf05f020
Lukas Prinzhorn <e11709481@student.tuwien.ac.at>
2020-05-12 13:20:25 +0200

    - Draft #3 of Detailed Market and Detailed Market Rate

--- app/src/main/java/com/example/weihnachtmaerkte/markets/DetailedMarketRateFragment.kt
+++ app/src/main/java/com/example/weihnachtmaerkte/markets/DetailedMarketRateFragment.kt
@@ -12,12 +12,13 @@ import androidx.navigation.fragment.findNavController
 import com.example.weihnachtmaerkte.R
 import com.example.weihnachtmaerkte.entities.Market
 
+
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class DetailedMarketRateFragment : Fragment() {
 
-    private lateinit var market : Market
+    private lateinit var market: Market
 
 
     override fun onCreateView(
@@ -31,20 +32,24 @@ class DetailedMarketRateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        view.findViewById<Button>(R.id.button_rate_go_back).setOnClickListener {
+            findNavController().navigate(R.id.action_Second2Fragment_to_First2Fragment)
+
+        }
+        view.findViewById<Button>(R.id.button_rate_save).setOnClickListener {
             findNavController().navigate(R.id.action_Second2Fragment_to_First2Fragment)
         }
-
         val bundle: Bundle? = activity?.intent?.getBundleExtra("bundle")
         val id: Long = bundle?.get("id") as Long
 
 
-        val markets : List<Market> = com.example.weihnachtmaerkte.backend.DataSource.createMarketsDataSet()
-        markets.forEach{
-            if (it.id == id){
+        val markets: List<Market> = com.example.weihnachtmaerkte.backend.DataSource.createMarketsDataSet()
+        markets.forEach {
+            if (it.id == id) {
                 market = it
             }
         }
+
         setData()
     }
 
@@ -52,8 +57,12 @@ class DetailedMarketRateFragment : Fragment() {
         var textView: TextView = view?.findViewById(R.id.detailed_market_name) as TextView
         textView.text = market.name
 
+        textView = view?.findViewById(R.id.detailed_market_rate_title) as TextView
+        val input: String = textView.text as String
+        val output = "$input '${market.name}'"
+        textView.text = output
 
-        val imageView : ImageView = view?.findViewById(R.id.detailed_market_image) as ImageView
+        val imageView: ImageView = view?.findViewById(R.id.detailed_market_image) as ImageView
 
         if (market.image.startsWith("@")) {
             if (market.image == "@drawable/museumsquartier") {
