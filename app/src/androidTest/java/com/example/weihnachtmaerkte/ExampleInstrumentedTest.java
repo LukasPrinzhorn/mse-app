8ad3dad837afda8c5a7c4dc917b45de13c457286
lukaskavan <lukas.kavan@gmail.com>
2020-06-11 13:31:06 +0200

    added first UI test, testing registration process

--- app/src/androidTest/java/com/example/weihnachtmaerkte/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/weihnachtmaerkte/ExampleInstrumentedTest.java
@@ -1,14 +1,35 @@
 package com.example.weihnachtmaerkte;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+
+import kotlin.jvm.JvmField;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,12 +37,47 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule(LoginActivity.class);
+
+    /*@Rule
+    @JvmField
+    public GrantPermissionRule grantPermissionRuleFineLocation = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Rule
+    @JvmField
+    public GrantPermissionRule grantPermissionRuleCoarseLocation = GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION);
+*/
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = getInstrumentation().getTargetContext();
 
         assertEquals("com.example.weihnachtmaerkte", appContext.getPackageName());
+    }
+
+    @Test
+    public void testRegistration(){
+        String username = "Testuser123";
+        onView(withId(R.id.username)).perform(typeText("Testuser123"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.login)).perform(click());
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            UiDevice device = UiDevice.getInstance(getInstrumentation());
+            UiObject allowPermissions = device.findObject(new UiSelector().text("Deny"));
+            if (allowPermissions.exists()) {
+                try {
+                    allowPermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        onView(withId(R.id.header_username)).check(matches(withText("Hallo " + username + "!")));
     }
 }
